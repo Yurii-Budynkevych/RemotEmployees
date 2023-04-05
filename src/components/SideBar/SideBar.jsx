fdcbf8613ea5a363a50c0f21b1acf1bbf7b74856
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-04-05 22:59:56 +0300

    SideBar add

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,11 +4,31 @@ export const SideBar = () => {
   return (
     <aside className="side-bar">
       <ul className="side-bar-list">
-        <li className="side-bar-item">Home</li>
-        <li className="side-bar-item">Recommendation</li>
-        <li className="side-bar-item">Explore</li>
-        <li className="side-bar-item">Help</li>
-        <li className="side-bar-item">About us</li>
+        <li className="side-bar-item">
+          <a className="side-bar-link side-bar-link__active" href="/">
+            Home
+          </a>
+        </li>
+        <li className="side-bar-item">
+          <a className="side-bar-link" href="/">
+            Recommendation
+          </a>
+        </li>
+        <li className="side-bar-item">
+          <a className="side-bar-link" href="/">
+            Explore
+          </a>
+        </li>
+        <li className="side-bar-item">
+          <a className="side-bar-link" href="/">
+            Help
+          </a>
+        </li>
+        <li className="side-bar-item">
+          <a className="side-bar-link" href="/">
+            About us
+          </a>
+        </li>
       </ul>
     </aside>
   );
