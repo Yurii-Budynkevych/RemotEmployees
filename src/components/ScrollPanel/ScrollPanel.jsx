261cceefa9968d43bb744c4dae63212031fdfc60
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-04-06 21:41:21 +0300

    scrollpanel fix

--- src/components/ScrollPanel/ScrollPanel.jsx
+++ src/components/ScrollPanel/ScrollPanel.jsx
@@ -1,5 +1,5 @@
 import "./ScrollPanel.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 
 export const ScrollPanel = () => {
@@ -19,27 +19,55 @@ export const ScrollPanel = () => {
     setpageCount(pageCount - 1);
   };
 
-  return (
-    <>
-      <aside className="featured-section-aside">
-        <h1 className="featured-section-title">Featured spots</h1>
-        <p className="featured-section-text">
-          Consequuntur eum alias quisquam dicta nihil ut.
-        </p>
-        <a className="featured-section-link" href="/">
-          View all
-        </a>
-      </aside>
-      <div className="featured-section-btnBox">
-        <button type="button" onClick={decr} className="featured-section-btn">
-          <AiFillCaretLeft size="2em" />
-        </button>
-        <button type="button" onClick={incr} className="featured-section-btn">
-          <AiFillCaretRight size="2em" />
-        </button>
-        <h2 className="featured-section-btnDescr">Outdoors edition</h2>
-      </div>
-      <p className="featured-section-pageCount">{pageCount}/2</p>
-    </>
-  );
+  if (pageCount === 1) {
+    return (
+      <>
+        <aside className="featured-section-aside">
+          <h1 className="featured-section-title">Featured spots</h1>
+          <p className="featured-section-text">
+            Consequuntur eum alias quisquam dicta nihil ut.
+          </p>
+          <a className="featured-section-link" href="/">
+            View all
+          </a>
+        </aside>
+        <div className="featured-section-btnBox">
+          <button type="button" onClick={decr} className="featured-section-btn">
+            <AiFillCaretLeft size="2em" />
+          </button>
+          <button type="button" onClick={incr} className="featured-section-btn">
+            <AiFillCaretRight size="2em" />
+          </button>
+          <h2 className="featured-section-btnDescr">Outdoors edition</h2>
+        </div>
+        <p className="featured-section-pageCount">{pageCount}/2</p>
+      </>
+    );
+  }
+
+  if (pageCount === 2) {
+    return (
+      <>
+        <aside className="featured-section-aside">
+          <h1 className="featured-section-title">Featured spots</h1>
+          <p className="featured-section-text">
+            Consequuntur eum alias quisquam dicta nihil ut.
+          </p>
+          <a className="featured-section-link" href="/">
+            View all
+          </a>
+        </aside>
+        <div className="featured-section-btnBox">
+          <button type="button" onClick={decr} className="featured-section-btn">
+            <AiFillCaretLeft size="2em" />
+          </button>
+          <button type="button" onClick={incr} className="featured-section-btn">
+            <AiFillCaretRight size="2em" />
+          </button>
+          <h2 className="featured-section-btnDescr">Outdoors edition</h2>
+        </div>
+        <p className="featured-section-pageCount">{pageCount}/2</p>
+      </>
+    );
+  }
 };
